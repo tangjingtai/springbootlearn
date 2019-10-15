a6f7a2251ae61d5b1f9fce3da8290106e78ee2a1
tangjingtai <a253210810@qq.com>
2019-10-15 23:29:03 +0800

    init

---
+++ src/main/java/com/jt/springbootlearn/SpringbootlearnApplication.java
@@ -0,0 +1,18 @@
+package com.jt.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class SpringbootlearnApplication {
+
+	public static void main(String[] args) {
+	    // Spring应用程序启动
+        SpringApplication.run(SpringbootlearnApplication.class, args);
+
+    }
+
+}
