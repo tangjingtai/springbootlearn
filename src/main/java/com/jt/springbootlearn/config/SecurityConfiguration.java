fabe88ec742eca90496cb5509a94d927471678d0
tangjingtai <a253210810@qq.com>
2019-12-01 22:52:25 +0800

    添加泛型擦除测试

--- src/main/java/com/jt/springbootlearn/config/SecurityConfiguration.java
+++ src/main/java/com/jt/springbootlearn/config/SecurityConfiguration.java
@@ -3,18 +3,15 @@ package com.jt.springbootlearn.config;
 import com.jt.springbootlearn.security.CsrfSecurityRequestMatcher;
 import com.jt.springbootlearn.security.MD5PasswordEncoder;
 import com.jt.springbootlearn.util.TokenProvider;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
